f59c6b903641e6891a8e6cb6263df8bae69dd13d
andrealangianese <andre.langia.01@gmail.com>
2026-02-20 15:01:31 +0100

    creo funzione di index e show base e le esporto

--- controllers/filmController.js
+++ controllers/filmController.js
@@ -1,3 +1,15 @@
 // importo connection e la salvo dal db.js
 
 const connection = require('../data/db')
+
+// funzione di index 
+function index(req, res){
+
+}
+
+// funzione di show 
+function show(req, res){
+
+}
+
+module.exports = {index, show}
