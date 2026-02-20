3bbaf8142aada414489b9c3b6c563a2e19f9e69b
andrealangianese <andre.langia.01@gmail.com>
2026-02-20 15:13:51 +0100

    importo relativo controller da usare

--- routers/filmRouter.js
+++ routers/filmRouter.js
@@ -3,3 +3,7 @@ const express = require('express');
 
 // utilizzo parte di express per gestire le rotte
 const router = express.Router();
+
+// importo relativo controller da usare
+
+const filmController = require('../controllers/filmController')
