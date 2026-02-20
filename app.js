cd28c781765559d5bcf6da6480e5ad0e92943a03
andrealangianese <andre.langia.01@gmail.com>
2026-02-20 14:05:43 +0100

    creo rotta home APP

--- app.js
+++ app.js
@@ -6,3 +6,8 @@ const port = 3000;
 
 // attivazione della cartella public per uso file statici
 app.use(express.static('public'));
+
+// creo rotta home APP
+app.get('/', (req, res) => {
+    res.send("<h1>Rotta di home dei movies</h1>")
+})
