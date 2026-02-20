34d6463c4b293b7101a830eb7157f50dad2bb528
andrealangianese <andre.langia.01@gmail.com>
2026-02-20 15:44:31 +0100

    eseguo query in index completa e testata

--- controllers/filmController.js
+++ controllers/filmController.js
@@ -5,7 +5,18 @@ const connection = require('../data/db')
 // funzione di index 
 function index(req, res){
 
-}
+//preparo la query
+const sql = 'SELECT * FROM movies'
+
+//eseguo la query
+connection.query(sql, (err, results) => {
+    if(err) {
+        console.error('Error executing query:', err);
+        res.status(500).json({ error: 'Server Error' });
+        return;
+    }
+    res.json(results);
+})}
 
 // funzione di show 
 function show(req, res){
