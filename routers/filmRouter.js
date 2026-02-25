52d0fadcaf0661998d98d08f123c31a5ddad53ae
andrealangianese <andre.langia.01@gmail.com>
2026-02-25 14:31:34 +0100

    aggiungo la rotta (post) nel router

--- routers/filmRouter.js
+++ routers/filmRouter.js
@@ -18,6 +18,10 @@ router.get('/' , filmController.index)
 
 router.get('/:id', filmController.show)
 
+// rotta per aggiungere una nuova review
+
+router.post('/:id/reviews', filmController.storeReview)
+
 //esporto le rotte
 
 module.exports = router
